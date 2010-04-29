24b2dbdde71b7c8b7c097768213ab6f78fc2cf82
Jonas Bonn <jonas@southpole.se>
2010-04-29 17:21:56 +0200

    Use generic bugs.h

--- arch/or32/include/asm/bugs.h
+++ arch/or32/include/asm/bugs.h
@@ -1,14 +1,1 @@
-/*
- * This is included by init/main.c to check for architecture-dependent bugs.
- *
- * Needs:
- *	void check_bugs(void);
- */
-
-static void check_bugs(void)
-{
-}
-
-
-
-
+#include <asm-generic/bugs.h>
