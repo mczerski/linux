ee9f3ebd3082dcb615e6caaa9a8bcb549f840ea9
Jonas Bonn <jonas@southpole.se>
2010-05-21 13:40:43 +0200

    unaligned.h: Use memmove instead of struct
    The OR32 GCC compiler doesn't seem to respect the packed attribute
    so we need to use the memmove variant of unaligned access.  This means we
    need to use a slightly modified version of the generic unaligned.h
    header.

--- arch/or32/include/asm/unaligned.h
+++ arch/or32/include/asm/unaligned.h
@@ -1,1 +1,33 @@
-#include <asm-generic/unaligned.h>
+#ifndef __ASM_OR32_UNALIGNED_H
+#define __ASM_OR32_UNALIGNED_H
+
+/*
+ * This is copied from the generic implementation and the C-struct
+ * variant replaced with the memmove variant.  The GCC compiler
+ * for the OR32 arch optimizes too aggressively for the C-struct
+ * variant to work, so use the memmove variant instead.
+ *
+ * It may be worth considering implementing the unaligned access
+ * exception handler and allowing unaligned accesses (access_ok.h)... 
+ * not sure if it would be much of a performance win without further
+ * investigation.
+ */
+#include <asm/byteorder.h>
+
+#if defined(__LITTLE_ENDIAN)
+# include <linux/unaligned/le_memmove.h>
+# include <linux/unaligned/be_byteshift.h>
+# include <linux/unaligned/generic.h>
+# define get_unaligned	__get_unaligned_le
+# define put_unaligned	__put_unaligned_le
+#elif defined(__BIG_ENDIAN)
+# include <linux/unaligned/be_memmove.h>
+# include <linux/unaligned/le_byteshift.h>
+# include <linux/unaligned/generic.h>
+# define get_unaligned	__get_unaligned_be
+# define put_unaligned	__put_unaligned_be
+#else
+# error need to define endianess
+#endif
+
+#endif /* __ASM_OR32_UNALIGNED_H */
