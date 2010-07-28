f83896035a6534ef0015a71e463f15088803456e
Jonas Bonn <jonas@southpole.se>
2010-07-28 13:09:37 +0200

    Fiddle with early serial stuff

--- arch/or32/include/asm/board.h
+++ arch/or32/include/asm/board.h
@@ -5,15 +5,15 @@
 #define SYS_CLK		(CONFIG_OR32_SYS_CLK*1000000)
 
 /* Devices base address */
-#define UART_BASE_ADD   0x90000000
+//#define UART_BASE_ADD   0x90000000
 
 /* Define this if you want to use I and/or D MMU */
 
 #define DMMU_SET_NB     CONFIG_OR32_DTLB_ENTRIES
 #define IMMU_SET_NB     CONFIG_OR32_ITLB_ENTRIES
 
-#define OR32_CONSOLE_BAUD  115200
-#define UART_DEVISOR       SYS_CLK/(16*OR32_CONSOLE_BAUD)
+//#define OR32_CONSOLE_BAUD  115200
+//#define UART_DEVISOR       SYS_CLK/(16*OR32_CONSOLE_BAUD)
 
 #endif /* _OR32_BOARH_H */
 
