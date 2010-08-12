863469d4becf8d49923357ab5e37d39911776df4
Jonas Bonn <jonas@southpole.se>
2010-08-12 15:09:31 +0200

    WIP

--- arch/or32/include/asm/posix_types.h
+++ arch/or32/include/asm/posix_types.h
@@ -5,8 +5,9 @@
  * definitions in the generic header.  For everything else, the generic
  * header applies.
  */
-/*
 
+/* We can get rid of these defines, but we need to change in libC also */
+/*
 typedef unsigned int    __kernel_ino_t;
 #define __kernel_ino_t __kernel_ino_t
 
@@ -18,9 +19,7 @@ typedef short           __kernel_ipc_pid_t;
 
 typedef unsigned short  __kernel_old_dev_t;
 #define __kernel_old_dev_t __kernel_old_dev_t
-
 */
-
 #include <asm-generic/posix_types.h>
 
 #endif /* __ASM_OPENRISC_POSIX_TYPES_H */
