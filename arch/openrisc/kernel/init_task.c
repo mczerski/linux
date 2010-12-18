ea040c49a6e33d6c39aa48b8665da76f535b2ca0
Jonas Bonn <jonas.bonn@gmail.com>
2010-12-19 00:34:36 +0100

    Cleanup init_task.c
    Fix up comment so that it resembles reality and remove a bunch of
    unused headers.

--- arch/openrisc/kernel/init_task.c
+++ arch/openrisc/kernel/init_task.c
@@ -1,13 +1,5 @@
-#include <linux/mm.h>
-#include <linux/module.h>
-#include <linux/fs.h>
-#include <linux/sched.h>
-#include <linux/init.h>
 #include <linux/init_task.h>
 #include <linux/mqueue.h>
-#include <linux/uaccess.h>
-
-#include <asm/pgtable.h>
 
 static struct signal_struct init_signals = INIT_SIGNALS(init_signals);
 static struct sighand_struct init_sighand = INIT_SIGHAND(init_sighand);
@@ -15,12 +7,9 @@ static struct sighand_struct init_sighand = INIT_SIGHAND(init_sighand);
 /*
  * Initial thread structure.
  *
- * We need to make sure that this is 8192-byte aligned due to the
- * way process stacks are handled. This is done by making sure
- * the linker maps this in the .text segment right after head.S,
- * and making head.S ensure the proper alignment.
- *
- * The things we do for performance..
+ * We need to make sure that this is THREAD_SIZE aligned due to the
+ * way process stacks are handled. This is done by having a special
+ * "init_task" linker map entry..
  */
 union thread_union init_thread_union __init_task_data =
 	{ INIT_THREAD_INFO(init_task) };
