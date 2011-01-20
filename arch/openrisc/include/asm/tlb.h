68efa5826ca4f0189c80b39e2b04388e289f7466
Jonas Bonn <jonas.bonn@gmail.com>
2011-01-20 15:39:58 +0100

    Build fix: add missing include

--- arch/openrisc/include/asm/tlb.h
+++ arch/openrisc/include/asm/tlb.h
@@ -10,6 +10,7 @@
 #define __tlb_remove_tlb_entry(tlb, ptep, address) do { } while (0)
 
 #define tlb_flush(tlb) flush_tlb_mm((tlb)->mm)
+#include <linux/pagemap.h>
 #include <asm-generic/tlb.h>
 
 #endif /* __ASM_OPENRISC_TLB_H__ */
