b52301a9a96ffe7bc480b7b0cba5a5df85421421
Giuseppe Scrivano <gscrivano@gnu.org>
2011-03-03 13:07:08 +0100

    Define io{read,write}{8,16,32}_rep
    Now {write,read}{l,w} respects the platform endianess.

--- arch/openrisc/include/asm/io.h
+++ arch/openrisc/include/asm/io.h
@@ -2,6 +2,7 @@
 #define __ASM_OPENRISC_IO_H
 
 #include <asm/page.h>   /* for __va, __pa */
+#include <asm/byteorder.h>
 
 
 /*
@@ -49,19 +50,20 @@ extern void iounmap(void *addr);
  * differently. On the openrisc architecture, we just read/write the
  * memory location directly.
  */
-#define readb(addr) (*(volatile unsigned char *) (addr))
-#define readw(addr) (*(volatile unsigned short *) (addr))
-#define readl(addr) (*(volatile unsigned int *) (addr))
-#define __raw_readb readb
-#define __raw_readw readw
-#define __raw_readl readl
 
-#define writeb(b,addr) ((*(volatile unsigned char *) (addr)) = (b))
-#define writew(b,addr) ((*(volatile unsigned short *) (addr)) = (b))
-#define writel(b,addr) ((*(volatile unsigned int *) (addr)) = (b))
-#define __raw_writeb writeb
-#define __raw_writew writew
-#define __raw_writel writel
+#define __raw_readb(addr) (*(volatile unsigned char *) (addr))
+#define __raw_readw(addr) (*(volatile unsigned short *) (addr))
+#define __raw_readl(addr) (*(volatile unsigned int *) (addr))
+#define readb __raw_readb
+#define readw(addr) (le16_to_cpu(__raw_readw(addr)))
+#define readl(addr) (le32_to_cpu(__raw_readl(addr)))
+
+#define __raw_writeb(b,addr) ((*(volatile unsigned char *) (addr)) = (b))
+#define __raw_writew(b,addr) ((*(volatile unsigned short *) (addr)) = (b))
+#define __raw_writel(b,addr) ((*(volatile unsigned int *) (addr)) = (b))
+#define writeb __raw_writeb
+#define writew(b,addr) __raw_writew(cpu_to_le16(b), addr)
+#define writel(b,addr) __raw_writel(cpu_to_le32(b), addr)
 
 #define memset_io(a,b,c)	memset((void *)(a),(b),(c))
 #define memcpy_fromio(a,b,c)	memcpy((a),(void *)(b),(c))
@@ -81,6 +83,56 @@ extern void iounmap(void *addr);
 
 #define inb_p(port)             inb((port))
 #define outb_p(val, port)       outb((val), (port))
+
+static inline void ioread8_rep(void __iomem *port, void *buf, unsigned long count)
+{
+	unsigned char *p = buf;
+	while (count--)
+		*p++ = readb(port);
+}
+
+static inline void ioread16_rep(void __iomem *port, void *buf, unsigned long count)
+{
+	unsigned short *p = buf;
+	while (count >= 2) {
+		*p++ = readw(port);
+		count -= 2;
+	}
+}
+
+static inline void ioread32_rep(void __iomem *port, void *buf, unsigned long count)
+{
+	unsigned int *p = buf;
+	while (count >= 4) {
+		*p++ = readl(port);
+		count -= 4;
+	}
+}
+
+static inline void iowrite8_rep(void __iomem *port, void *buf, unsigned long count)
+{
+	unsigned char *p = buf;
+	while (count--)
+		writeb(*p++, port);
+}
+
+static inline void iowrite16_rep(void __iomem *port, void *buf, unsigned long count)
+{
+	unsigned short *p = buf;
+	while (count >= 2) {
+		writew(*p++, port);
+		count -= 2;
+	}
+}
+
+static inline void iowrite32_rep(void __iomem *port, void *buf, unsigned long count)
+{
+	unsigned int *p = buf;
+	while (count >= 4) {
+		writel(*p++, port);
+		count -= 4;
+	}
+}
 
 /*
  * Convert a physical pointer to a virtual kernel pointer for /dev/mem
