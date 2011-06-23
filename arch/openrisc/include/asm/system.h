4a9d2e5983a9d32eabef5d89378ff4f4d94ef6c9
Jonas Bonn <jonas@southpole.se>
2011-06-23 20:45:01 +0200

    headers: create spr.h

--- arch/openrisc/include/asm/system.h
+++ arch/openrisc/include/asm/system.h
@@ -19,32 +19,11 @@
 #ifndef __ASM_OPENRISC_SYSTEM_H
 #define __ASM_OPENRISC_SYSTEM_H
 
-#include <asm-generic/system.h>
-
 #ifdef __KERNEL__
 #ifndef __ASSEMBLY__
 
-#define mtspr(_spr, _val) __asm__ __volatile__ ( 		\
-	"l.mtspr r0,%1,%0"					\
-	:: "K" (_spr), "r" (_val))
-#define mtspr_off(_spr, _off, _val) __asm__ __volatile__ ( 	\
-	"l.mtspr %0,%1,%2"					\
-	:: "r" (_off), "r" (_val), "K" (_spr))
-
-static inline unsigned long mfspr(unsigned long add)
-{
-	unsigned long ret;
-	__asm__ __volatile__ ("l.mfspr %0,r0,%1" : "=r" (ret) : "K" (add));
-	return ret;
-}
-
-static inline unsigned long mfspr_off(unsigned long add, unsigned long offset)
-{
-	unsigned long ret;
-	__asm__ __volatile__ ("l.mfspr %0,%1,%2" : "=r" (ret)
-						 : "r" (offset), "K" (add));
-	return ret;
-}
+#include <asm/spr.h>
+#include <asm-generic/system.h>
 
 /* We probably need this definition, but the generic system.h provides it
  * and it's not used on our arch anyway...
