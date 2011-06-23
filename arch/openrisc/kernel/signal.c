1148537bb08edbde75b9c5c6f1660e3c3d562d94
Jonas Bonn <jonas@southpole.se>
2011-06-24 00:40:07 +0200

    signal: Lindent signal.c

--- arch/openrisc/kernel/signal.c
+++ arch/openrisc/kernel/signal.c
@@ -38,7 +38,7 @@
 #define _BLOCKABLE (~(sigmask(SIGKILL) | sigmask(SIGSTOP)))
 
 asmlinkage long
-_sys_sigaltstack(const stack_t *uss, stack_t *uoss, struct pt_regs *regs)
+_sys_sigaltstack(const stack_t * uss, stack_t * uoss, struct pt_regs *regs)
 {
 	return do_sigaltstack(uss, uoss, regs->sp);
 }
@@ -48,9 +48,8 @@ struct rt_sigframe {
 	void *puc;
 	struct siginfo info;
 	struct ucontext uc;
-	unsigned char retcode[16];  /* trampoline code */
+	unsigned char retcode[16];	/* trampoline code */
 };
-
 
 static int restore_sigcontext(struct pt_regs *regs, struct sigcontext *sc)
 {
@@ -66,7 +65,7 @@ static int restore_sigcontext(struct pt_regs *regs, struct sigcontext *sc)
 	 */
 
 	if (__copy_from_user(regs, sc, sizeof(struct pt_regs)))
-                goto badframe;
+		goto badframe;
 
 	/* make sure the SM-bit is cleared so user-mode cannot fool us */
 	regs->sr &= ~SPR_SR_SM;
@@ -91,20 +90,19 @@ badframe:
 	return 1;
 }
 
-asmlinkage long
-_sys_rt_sigreturn(struct pt_regs *regs)
+asmlinkage long _sys_rt_sigreturn(struct pt_regs *regs)
 {
 	struct rt_sigframe *frame = (struct rt_sigframe __user *)regs->sp;
 	sigset_t set;
 	stack_t st;
 
-        /*
-         * Since we stacked the signal on a dword boundary,
-         * then frame should be dword aligned here.  If it's
-         * not, then the user is trying to mess with us.
-         */
-        if (((long)frame) & 3)
-                goto badframe;
+	/*
+	 * Since we stacked the signal on a dword boundary,
+	 * then frame should be dword aligned here.  If it's
+	 * not, then the user is trying to mess with us.
+	 */
+	if (((long)frame) & 3)
+		goto badframe;
 
 	if (!access_ok(VERIFY_READ, frame, sizeof(*frame)))
 		goto badframe;
@@ -158,8 +156,7 @@ static int setup_sigcontext(struct sigcontext *sc, struct pt_regs *regs,
 	return err;
 }
 
-static inline unsigned long
-align_sigframe(unsigned long sp)
+static inline unsigned long align_sigframe(unsigned long sp)
 {
 	return (sp & ~3UL);
 }
@@ -169,8 +166,8 @@ align_sigframe(unsigned long sp)
  * or the alternate stack.
  */
 
-static inline void __user *
-get_sigframe(struct k_sigaction *ka, struct pt_regs * regs, size_t frame_size)
+static inline void __user *get_sigframe(struct k_sigaction *ka,
+					struct pt_regs *regs, size_t frame_size)
 {
 	unsigned long sp = regs->sp;
 	int onsigstack = on_sig_stack(sp);
@@ -203,8 +200,8 @@ get_sigframe(struct k_sigaction *ka, struct pt_regs * regs, size_t frame_size)
  * trampoline which performs the syscall sigreturn, or a provided
  * user-mode trampoline.
  */
-static void setup_rt_frame(int sig, struct k_sigaction *ka, siginfo_t *info,
-			   sigset_t *set, struct pt_regs * regs)
+static void setup_rt_frame(int sig, struct k_sigaction *ka, siginfo_t * info,
+			   sigset_t * set, struct pt_regs *regs)
 {
 	struct rt_sigframe *frame;
 	unsigned long return_ip;
@@ -224,14 +221,13 @@ static void setup_rt_frame(int sig, struct k_sigaction *ka, siginfo_t *info,
 		goto give_sigsegv;
 
 	/* Clear all the bits of the ucontext we don't use.  */
-        err |= __clear_user(&frame->uc, offsetof(struct ucontext, uc_mcontext));
-        err |= __put_user(0, &frame->uc.uc_flags);
-        err |= __put_user(NULL, &frame->uc.uc_link);
-        err |= __put_user((void *)current->sas_ss_sp,
-                        &frame->uc.uc_stack.ss_sp);
-        err |= __put_user(sas_ss_flags(regs->sp),
-                        &frame->uc.uc_stack.ss_flags);
-        err |= __put_user(current->sas_ss_size, &frame->uc.uc_stack.ss_size);
+	err |= __clear_user(&frame->uc, offsetof(struct ucontext, uc_mcontext));
+	err |= __put_user(0, &frame->uc.uc_flags);
+	err |= __put_user(NULL, &frame->uc.uc_link);
+	err |= __put_user((void *)current->sas_ss_sp,
+			  &frame->uc.uc_stack.ss_sp);
+	err |= __put_user(sas_ss_flags(regs->sp), &frame->uc.uc_stack.ss_flags);
+	err |= __put_user(current->sas_ss_size, &frame->uc.uc_stack.ss_size);
 	err |= setup_sigcontext(&frame->uc.uc_mcontext, regs, set->sig[0]);
 
 	err |= __copy_to_user(&frame->uc.uc_sigmask, set, sizeof(*set));
@@ -242,10 +238,10 @@ static void setup_rt_frame(int sig, struct k_sigaction *ka, siginfo_t *info,
 	/* trampoline - the desired return ip is the retcode itself */
 	return_ip = (unsigned long)&frame->retcode;
 	/* This is l.ori r11,r0,__NR_sigreturn, l.sys 1 */
-	err |= __put_user(0xa960        , (short *)(frame->retcode+0));
-	err |= __put_user(__NR_rt_sigreturn, (short *)(frame->retcode+2));
-	err |= __put_user(0x20000001, (unsigned long *)(frame->retcode+4));
-	err |= __put_user(0x15000000, (unsigned long *)(frame->retcode+8));
+	err |= __put_user(0xa960, (short *)(frame->retcode + 0));
+	err |= __put_user(__NR_rt_sigreturn, (short *)(frame->retcode + 2));
+	err |= __put_user(0x20000001, (unsigned long *)(frame->retcode + 4));
+	err |= __put_user(0x15000000, (unsigned long *)(frame->retcode + 8));
 
 	if (err)
 		goto give_sigsegv;
@@ -253,11 +249,11 @@ static void setup_rt_frame(int sig, struct k_sigaction *ka, siginfo_t *info,
 	/* TODO what is the current->exec_domain stuff and invmap ? */
 
 	/* Set up registers for signal handler */
-	regs->pc = (unsigned long) ka->sa.sa_handler; /* what we enter NOW   */
-	regs->gpr[9] = (unsigned long) return_ip;    /* what we enter LATER */
-	regs->gpr[3] = (unsigned long) sig;          /* arg 1: signo */
-        regs->gpr[4] = (unsigned long) &frame->info; /* arg 2: (siginfo_t*) */
-        regs->gpr[5] = (unsigned long) &frame->uc;   /* arg 3: ucontext */
+	regs->pc = (unsigned long)ka->sa.sa_handler;	/* what we enter NOW   */
+	regs->gpr[9] = (unsigned long)return_ip;	/* what we enter LATER */
+	regs->gpr[3] = (unsigned long)sig;	/* arg 1: signo */
+	regs->gpr[4] = (unsigned long)&frame->info;	/* arg 2: (siginfo_t*) */
+	regs->gpr[5] = (unsigned long)&frame->uc;	/* arg 3: ucontext */
 
 	/* actually move the usp to reflect the stacked frame */
 	regs->sp = (unsigned long)frame;
@@ -272,8 +268,8 @@ give_sigsegv:
 
 static inline void
 handle_signal(unsigned long sig,
-	      siginfo_t *info, struct k_sigaction *ka,
-	      sigset_t *oldset, struct pt_regs * regs)
+	      siginfo_t * info, struct k_sigaction *ka,
+	      sigset_t * oldset, struct pt_regs *regs)
 {
 	setup_rt_frame(sig, ka, info, oldset, regs);
 
@@ -332,7 +328,7 @@ void do_signal(struct pt_regs *regs)
 	if (regs->syscallno) {
 		int restart = 0;
 
-		switch(regs->gpr[11]) {
+		switch (regs->gpr[11]) {
 		case -ERESTART_RESTARTBLOCK:
 		case -ERESTARTNOHAND:
 			/* Restart if there is no signal handler */
@@ -368,7 +364,7 @@ void do_signal(struct pt_regs *regs)
 			sigprocmask(SIG_SETMASK, &current->saved_sigmask, NULL);
 		}
 
-	} else { /* signr > 0 */
+	} else {		/* signr > 0 */
 		sigset_t *oldset;
 
 		if (current_thread_info()->flags & _TIF_RESTORE_SIGMASK)
@@ -394,12 +390,10 @@ void do_signal(struct pt_regs *regs)
 	return;
 }
 
-asmlinkage void
-do_notify_resume(struct pt_regs *regs)
+asmlinkage void do_notify_resume(struct pt_regs *regs)
 {
 	if (current_thread_info()->flags & _TIF_SIGPENDING)
 		do_signal(regs);
-
 
 	if (current_thread_info()->flags & _TIF_NOTIFY_RESUME) {
 		clear_thread_flag(TIF_NOTIFY_RESUME);
