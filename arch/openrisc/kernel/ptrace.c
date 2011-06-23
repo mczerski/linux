a50cc9b2f1206c6d2c84d3f1336c500f7ab2924f
Jonas Bonn <jonas@southpole.se>
2011-06-24 00:37:46 +0200

    ptrace: Lindent ptrace.c

--- arch/openrisc/kernel/ptrace.c
+++ arch/openrisc/kernel/ptrace.c
@@ -44,7 +44,7 @@
 static int genregs_get(struct task_struct *target,
 		       const struct user_regset *regset,
 		       unsigned int pos, unsigned int count,
-		       void *kbuf, void __user *ubuf)
+		       void *kbuf, void __user * ubuf)
 {
 	const struct pt_regs *regs = task_pt_regs(target);
 	int ret;
@@ -52,7 +52,7 @@ static int genregs_get(struct task_struct *target,
 #if 0
 	/* r0 */
 	ret = user_regset_copyout_zero(&pos, &count, &kbuf, &ubuf,
-					0, offsetof(struct pt_regs, regs));
+				       0, offsetof(struct pt_regs, regs));
 #endif
 
 	ret = user_regset_copyout(&pos, &count, &kbuf, &ubuf,
@@ -64,7 +64,7 @@ static int genregs_get(struct task_struct *target,
 	/* fill out rest of elf_gregset_t structure with zeroes */
 	if (!ret)
 		ret = user_regset_copyout_zero(&pos, &count, &kbuf, &ubuf,
-						sizeof(struct pt_regs), -1);
+					       sizeof(struct pt_regs), -1);
 #endif
 
 	return ret;
@@ -76,15 +76,14 @@ static int genregs_get(struct task_struct *target,
 static int genregs_set(struct task_struct *target,
 		       const struct user_regset *regset,
 		       unsigned int pos, unsigned int count,
-		       const void *kbuf, const void __user *ubuf)
+		       const void *kbuf, const void __user * ubuf)
 {
 	struct pt_regs *regs = task_pt_regs(target);
 	int ret;
 
 	/* PC */
 	ret = user_regset_copyin(&pos, &count, &kbuf, &ubuf,
-				  regs,
-				  0, offsetof(struct pt_regs, sr));
+				 regs, 0, offsetof(struct pt_regs, sr));
 
 	/* skip SR */
 	ret = user_regset_copyin_ignore(&pos, &count, &kbuf, &ubuf,
@@ -93,9 +92,9 @@ static int genregs_set(struct task_struct *target,
 
 	/* SP, r2 - r31 */
 	ret = user_regset_copyin(&pos, &count, &kbuf, &ubuf,
-				  regs,
-				  offsetof(struct pt_regs, sp),
-				  sizeof(struct pt_regs));
+				 regs,
+				 offsetof(struct pt_regs, sp),
+				 sizeof(struct pt_regs));
 
 #if 0
 	/* read out the rest of the elf_gregset_t structure */
@@ -116,20 +115,20 @@ enum openrisc_regset {
 
 static const struct user_regset openrisc_regsets[] = {
 	[REGSET_GENERAL] = {
-		.core_note_type	= NT_PRSTATUS,
-		.n		= ELF_NGREG,
-		.size		= sizeof(long),
-		.align		= sizeof(long),
-		.get		= genregs_get,
-		.set		= genregs_set,
-	},
+			    .core_note_type = NT_PRSTATUS,
+			    .n = ELF_NGREG,
+			    .size = sizeof(long),
+			    .align = sizeof(long),
+			    .get = genregs_get,
+			    .set = genregs_set,
+			    },
 };
 
 static const struct user_regset_view user_openrisc_native_view = {
-	.name		= "OpenRISC",
-	.e_machine	= EM_OPENRISC,
-	.regsets	= openrisc_regsets,
-	.n		= ARRAY_SIZE(openrisc_regsets),
+	.name = "OpenRISC",
+	.e_machine = EM_OPENRISC,
+	.regsets = openrisc_regsets,
+	.n = ARRAY_SIZE(openrisc_regsets),
 };
 
 const struct user_regset_view *task_user_regset_view(struct task_struct *task)
@@ -147,13 +146,10 @@ void user_disable_single_step(struct task_struct *child)
 	clear_tsk_thread_flag(child, TIF_SINGLESTEP);
 }
 
-
 /*
  * does not yet catch signals sent when the child dies.
  * in exit.c or in signal.c.
  */
-
-
 
 #define OPC_MASK 0x3f
 #define OPC_SHIFT 26
@@ -182,8 +178,7 @@ void user_disable_single_step(struct task_struct *child)
    place a l.trap on. If we should take a branch, we must remember this.
 */
 static unsigned long
-get_next_address(struct task_struct *tsk, unsigned long pc,
-		 unsigned long insn)
+get_next_address(struct task_struct *tsk, unsigned long pc, unsigned long insn)
 {
 	struct pt_regs *regs;
 	struct debug_info *dbg;
@@ -197,30 +192,29 @@ get_next_address(struct task_struct *tsk, unsigned long pc,
 	/* Extract opcode from instruction */
 	opc = (insn >> OPC_SHIFT) & OPC_MASK;
 
-	pr_debug(KERN_INFO "ptrace get_next_address: pc 0x%.8lx insn. 0x%.8lx opc. 0x%.2x\n",
-		pc, insn, opc);
+	pr_debug(KERN_INFO
+		 "ptrace get_next_address: pc 0x%.8lx insn. 0x%.8lx opc. 0x%.2x\n",
+		 pc, insn, opc);
 
 	/*
 	   Will always proceed to next instruction if this function was called
 	   ie. we know we're not in a delay slot - the function calling this
 	   checks dbg->branch_taken.
-	*/
+	 */
 	npc = pc + 4;
 
 	/* Check if we're to record a branch */
 	switch (opc) {
 	case OPC_BNF:
 		/* Check flag - we're branching if !flag */
-		if (!(((regs)->sr) & SPR_SR_F))
-		{
+		if (!(((regs)->sr) & SPR_SR_F)) {
 			dbg->bp.branch = 1;
 			dbg->bp.branch_target = pc + BRANCH_OFFSET(insn);
 		}
 		break;
 	case OPC_BF:
 		/* Check flag - we're branching if flag */
-		if (((regs)->sr) & SPR_SR_F)
-		{
+		if (((regs)->sr) & SPR_SR_F) {
 			dbg->bp.branch = 1;
 			dbg->bp.branch_target = pc + BRANCH_OFFSET(insn);
 		}
@@ -237,12 +231,18 @@ get_next_address(struct task_struct *tsk, unsigned long pc,
 		/* Register number holding branch target is encoded in rB slot
 		   of instruction. Extract it. */
 		rB = (insn >> 11) & 0x1f;
-		pr_debug(KERN_INFO "ptrace get_next_address: jump reg from r%ld=0x%08lx\n",
-		       rB,/* *((unsigned long*)((char*)regs + (rB<<2) + 4))*/ regs->gprs[rB-2]);
+		pr_debug(KERN_INFO
+			 "ptrace get_next_address: jump reg from r%ld=0x%08lx\n",
+			 rB,
+			 /* *((unsigned long*)((char*)regs + (rB<<2) + 4)) */
+			 regs->gprs[rB - 2]);
 		if (rB < 2)
-			pr_debug(KERN_WARNING "ptrace get_next_address(): Warning, JR with GPR < 2");
+			pr_debug(KERN_WARNING
+				 "ptrace get_next_address(): Warning, JR with GPR < 2");
 		dbg->bp.branch = 1;
-		dbg->bp.branch_target = /* *((unsigned long*)((char*)regs + (rB<<2) + 4))*/regs->gprs[rB-2];
+		dbg->bp.branch_target =
+		    /* *((unsigned long*)((char*)regs + (rB<<2) + 4)) */
+		    regs->gprs[rB - 2];
 		break;
 #if 0
 	case OPC_SYSC:
@@ -260,14 +260,16 @@ get_next_address(struct task_struct *tsk, unsigned long pc,
 		dbg->bp.branch_insn_address = pc;
 
 	if (dbg->bp.branch)
-		pr_debug(KERN_INFO "ptrace get_next_address: branch detected to 0x%.8lx\n",dbg->bp.branch_target);
-	pr_debug(KERN_INFO "ptrace get_next_address: returning 0x%.8lx\n",npc);
+		pr_debug(KERN_INFO
+			 "ptrace get_next_address: branch detected to 0x%.8lx\n",
+			 dbg->bp.branch_target);
+	pr_debug(KERN_INFO "ptrace get_next_address: returning 0x%.8lx\n", npc);
 
 	return npc;
 }
 
 static inline int
-read_instr(struct task_struct *tsk, unsigned long addr, u32 *res)
+read_instr(struct task_struct *tsk, unsigned long addr, u32 * res)
 {
 	int ret;
 	u32 val;
@@ -276,7 +278,6 @@ read_instr(struct task_struct *tsk, unsigned long addr, u32 *res)
 	*res = val;
 	return ret;
 }
-
 
 static int
 swap_insn(struct task_struct *tsk, unsigned long addr,
@@ -299,15 +300,15 @@ add_breakpoint(struct task_struct *tsk, struct debug_info *dbg,
 
 	res = swap_insn(tsk, addr, &dbg->bp.insn, &new_insn, 4);
 
-	pr_debug(KERN_INFO "ptrace add_breakpoint: addr 0x%.8lx insn 0x%.8lx %d\n\n", addr,
-	       dbg->bp.insn, res);
+	pr_debug(KERN_INFO
+		 "ptrace add_breakpoint: addr 0x%.8lx insn 0x%.8lx %d\n\n",
+		 addr, dbg->bp.insn, res);
 
 	if (res == 4) {
 		dbg->bp.address = addr;
 		dbg->bp.set = 1;
 	}
 }
-
 
 /*
  * Clear the breakpoint in the user program.
@@ -322,35 +323,36 @@ static void clear_breakpoint(struct task_struct *tsk, struct debug_entry *bp)
 	unsigned long addr = bp->address;
 
 	regs = task_pt_regs(tsk);
-	pc = instruction_pointer(regs); /* This is NPC */
+	pc = instruction_pointer(regs);	/* This is NPC */
 
 	// Either at the breakpoint, or in delay slot (pc will be at address of
 	// branch instruction - OR1K exception handlers do that, for now.
 	// Perhaps FIXME
-	if ((pc == addr) ||
-	    ((bp->branch_insn_address == pc) && bp->branch))
-		ret = swap_insn(tsk, addr & ~3, &old_insn,
-				&bp->insn, 4);
-	else
-	{
+	if ((pc == addr) || ((bp->branch_insn_address == pc) && bp->branch))
+		ret = swap_insn(tsk, addr & ~3, &old_insn, &bp->insn, 4);
+	else {
 		// Trapped for some other reason.
-		pr_debug(KERN_INFO "ptrace clear_breakpoint: not correct, pc 0x%.8lx bp 0x%.8lx\n",
-		       pc, addr);
-		pr_debug(KERN_INFO "ptrace clear_breakpoint: 0x%.8lx 0x%.8lx %ld\n",
-		       bp->branch_insn_address, bp->branch_target, bp->branch);
+		pr_debug(KERN_INFO
+			 "ptrace clear_breakpoint: not correct, pc 0x%.8lx bp 0x%.8lx\n",
+			 pc, addr);
+		pr_debug(KERN_INFO
+			 "ptrace clear_breakpoint: 0x%.8lx 0x%.8lx %ld\n",
+			 bp->branch_insn_address, bp->branch_target,
+			 bp->branch);
 		return;
 	}
 
-	pr_debug(KERN_INFO "ptrace clear_breakpoint: addr 0x%.8lx insn 0x%.8lx %d\n", addr,
-	       bp->insn, ret);
+	pr_debug(KERN_INFO
+		 "ptrace clear_breakpoint: addr 0x%.8lx insn 0x%.8lx %d\n",
+		 addr, bp->insn, ret);
 
 	if (ret != 4 || old_insn != OR1K_TRAP)
-	  pr_debug(KERN_ERR "%s:%d: corrupted breakpoint at 0x%08lx (0x%08x)\n", tsk->comm,
-			task_pid_nr(tsk), addr, old_insn);
+		pr_debug(KERN_ERR
+			 "%s:%d: corrupted breakpoint at 0x%08lx (0x%08x)\n",
+			 tsk->comm, task_pid_nr(tsk), addr, old_insn);
 
 	bp->set = 0;
 }
-
 
 void ptrace_set_bpt(struct task_struct *tsk)
 {
@@ -360,7 +362,7 @@ void ptrace_set_bpt(struct task_struct *tsk)
 	int res;
 
 	regs = task_pt_regs(tsk);
-	pc = instruction_pointer(regs); /* This is NPC */
+	pc = instruction_pointer(regs);	/* This is NPC */
 
 	res = read_instr(tsk, pc, &insn);
 	if (!res) {
@@ -375,13 +377,12 @@ void ptrace_set_bpt(struct task_struct *tsk)
 		   target.
 		   This way we skip everything that goes on during an exception
 		   and can hopefully just track the program's execution.
-		*/
+		 */
 		if (dbg->bp.set)
 			return;
 
 		/* Are we in delay slot of branch we should take? */
-		if (dbg->bp.branch == 1)
-		{
+		if (dbg->bp.branch == 1) {
 			/* Branch target is already determined. Set l.trap. */
 			npc = dbg->bp.branch_target;
 
@@ -391,9 +392,7 @@ void ptrace_set_bpt(struct task_struct *tsk)
 
 			/* Now mark this as no longer needing to be taken */
 			dbg->bp.branch = 0;
-		}
-		else
-		{
+		} else {
 			npc = get_next_address(tsk, pc, insn);
 		}
 
@@ -401,17 +400,15 @@ void ptrace_set_bpt(struct task_struct *tsk)
 	}
 }
 
-
 /*
  * Ensure no single-step breakpoint is pending.  Returns non-zero
  * value if child was being single-stepped.
  */
 void ptrace_cancel_bpt(struct task_struct *tsk)
 {
-	clear_breakpoint(tsk, (struct debug_entry*) &current_thread_info()->debug);
+	clear_breakpoint(tsk,
+			 (struct debug_entry *)&current_thread_info()->debug);
 }
-
-
 
 /*
  * Called by kernel/ptrace.c when detaching..
@@ -426,19 +423,18 @@ void ptrace_disable(struct task_struct *child)
 	clear_tsk_thread_flag(child, TIF_SYSCALL_TRACE);
 }
 
-
 /*
  * Read the word at offset "off" into the "struct user".  We
  * actually access the pt_regs stored on the kernel stack.
  */
 static int ptrace_read_user(struct task_struct *tsk, unsigned long off,
-                            unsigned long __user *ret)
+			    unsigned long __user * ret)
 {
-	struct pt_regs* regs;
+	struct pt_regs *regs;
 	unsigned long tmp;
 
-  /*	if (off & 3 || off >= sizeof(struct user))
-      return -EIO;*/
+	/*    if (off & 3 || off >= sizeof(struct user))
+	   return -EIO; */
 
 	regs = task_pt_regs(tsk);
 
@@ -450,7 +446,7 @@ static int ptrace_read_user(struct task_struct *tsk, unsigned long off,
 	else if (off == PT_TEXT_END_ADDR)
 		tmp = tsk->mm->end_code;
 	else if (off < sizeof(struct pt_regs)) {
-		tmp = *((unsigned long*)((char*)regs + off));
+		tmp = *((unsigned long *)((char *)regs + off));
 	}
 
 	return put_user(tmp, ret);
@@ -461,12 +457,12 @@ static int ptrace_read_user(struct task_struct *tsk, unsigned long off,
  * actually access the pt_regs stored on the kernel stack.
  */
 static int ptrace_write_user(struct task_struct *tsk, unsigned long off,
-                             unsigned long val)
+			     unsigned long val)
 {
-	struct pt_regs* regs;
+	struct pt_regs *regs;
 
 	/*if (off & 3 || off >= sizeof(struct user))
-		return -EIO;*/
+	   return -EIO; */
 
 	if (off >= sizeof(struct pt_regs))
 		return 0;
@@ -474,7 +470,7 @@ static int ptrace_write_user(struct task_struct *tsk, unsigned long off,
 	regs = task_pt_regs(tsk);
 
 	if (off != offsetof(struct pt_regs, sr)) {
-		*((unsigned long*)((char*)regs + off)) = val;
+		*((unsigned long *)((char *)regs + off)) = val;
 	} else {
 		/* Prevent any process from setting the SR flags and
 		 * thus elevating privileges
@@ -485,13 +481,13 @@ static int ptrace_write_user(struct task_struct *tsk, unsigned long off,
 }
 
 long arch_ptrace(struct task_struct *child, long request, unsigned long addr,
-	         unsigned long data)
+		 unsigned long data)
 {
 	int ret;
 	unsigned long __user *datap = (unsigned long __user *)data;
 
 	switch (request) {
-	/* read the word at location addr in the USER area. */
+		/* read the word at location addr in the USER area. */
 	case PTRACE_PEEKUSR:
 		ret = ptrace_read_user(child, addr, datap);
 		break;
@@ -511,8 +507,7 @@ long arch_ptrace(struct task_struct *child, long request, unsigned long addr,
 /* notification of system call entry/exit
  * - triggered by current->work.syscall_trace
  */
-asmlinkage long
-do_syscall_trace_enter(struct pt_regs *regs)
+asmlinkage long do_syscall_trace_enter(struct pt_regs *regs)
 {
 	long ret = 0;
 
@@ -523,7 +518,7 @@ do_syscall_trace_enter(struct pt_regs *regs)
 		 * We'll return a bogus call number to get an ENOSYS
 		 * error, but leave the original number in <something>.
 		 */
-                ret = -1L;
+		ret = -1L;
 
 /*	if (unlikely(test_thread_flag(TIF_SYSCALL_TRACEPOINT)))
 		trace_sys_enter(regs, regs->syscallno);
@@ -535,11 +530,10 @@ do_syscall_trace_enter(struct pt_regs *regs)
 				    regs->gpr[3], regs->gpr[4],
 				    regs->gpr[5], regs->gpr[6]);
 
-	return ret ?: regs->syscallno;
+	return ret ? : regs->syscallno;
 
 #if 0
 /*FIXME : audit the rest of this */
-
 
 	/*
 	 * this isn't the same as continuing with a signal, but it will do
@@ -550,19 +544,18 @@ do_syscall_trace_enter(struct pt_regs *regs)
 		send_sig(current->exit_code, current, 1);
 		current->exit_code = 0;
 	}
- out:
+out:
 	/*FIXME: audit_arch isn't even defined for openrisc */
 	/*FIXME:  What's with the register numbers here... makes no sense */
 	if (unlikely(current->audit_context) && !entryexit)
 		audit_syscall_entry(audit_arch(), regs->regs[2],
 				    regs->regs[4], regs->regs[5],
-				    regs->regs[6], regs->regs[7]);/*RGD*/
-
+				    regs->regs[6], regs->regs[7]);
+	/*RGD*/
 #endif
 }
 
-asmlinkage void
-do_syscall_trace_leave(struct pt_regs* regs)
+asmlinkage void do_syscall_trace_leave(struct pt_regs *regs)
 {
 	int step;
 
