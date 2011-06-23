a2e7ab6a229fc557d19973ab821673fd546c0af0
Jonas Bonn <jonas@southpole.se>
2011-06-24 00:33:18 +0200

    time: Lindent time.c

--- arch/openrisc/kernel/time.c
+++ arch/openrisc/kernel/time.c
@@ -28,7 +28,7 @@
 #include <asm/cpuinfo.h>
 
 static int openrisc_timer_set_next_event(unsigned long delta,
-                                     struct clock_event_device *dev)
+					 struct clock_event_device *dev)
 {
 	u32 c;
 
@@ -45,30 +45,30 @@ static int openrisc_timer_set_next_event(unsigned long delta,
 	 */
 	mtspr(SPR_TTMR, SPR_TTMR_CR | SPR_TTMR_IE | c);
 
-        return 0;
+	return 0;
 }
 
 static void openrisc_timer_set_mode(enum clock_event_mode mode,
-                                struct clock_event_device *evt)
+				    struct clock_event_device *evt)
 {
-        switch (mode) {
-        case CLOCK_EVT_MODE_PERIODIC:
-                pr_debug(KERN_INFO "%s: periodic\n", __func__);
+	switch (mode) {
+	case CLOCK_EVT_MODE_PERIODIC:
+		pr_debug(KERN_INFO "%s: periodic\n", __func__);
 		BUG();
-                break;
-        case CLOCK_EVT_MODE_ONESHOT:
-                pr_debug(KERN_INFO "%s: oneshot\n", __func__);
-                break;
-        case CLOCK_EVT_MODE_UNUSED:
-                pr_debug(KERN_INFO "%s: unused\n", __func__);
-                break;
-        case CLOCK_EVT_MODE_SHUTDOWN:
-                pr_debug(KERN_INFO "%s: shutdown\n", __func__);
-                break;
-        case CLOCK_EVT_MODE_RESUME:
-                pr_debug(KERN_INFO "%s: resume\n", __func__);
-                break;
-        }
+		break;
+	case CLOCK_EVT_MODE_ONESHOT:
+		pr_debug(KERN_INFO "%s: oneshot\n", __func__);
+		break;
+	case CLOCK_EVT_MODE_UNUSED:
+		pr_debug(KERN_INFO "%s: unused\n", __func__);
+		break;
+	case CLOCK_EVT_MODE_SHUTDOWN:
+		pr_debug(KERN_INFO "%s: shutdown\n", __func__);
+		break;
+	case CLOCK_EVT_MODE_RESUME:
+		pr_debug(KERN_INFO "%s: resume\n", __func__);
+		break;
+	}
 }
 
 /* This is the clock event device based on the OR1K tick timer.
@@ -78,11 +78,11 @@ static void openrisc_timer_set_mode(enum clock_event_mode mode,
  */
 
 static struct clock_event_device clockevent_openrisc_timer = {
-        .name           = "openrisc_timer_clockevent",
-        .features       = CLOCK_EVT_FEAT_ONESHOT,
-        .rating         = 300,
-        .set_next_event = openrisc_timer_set_next_event,
-        .set_mode       = openrisc_timer_set_mode,
+	.name = "openrisc_timer_clockevent",
+	.features = CLOCK_EVT_FEAT_ONESHOT,
+	.rating = 300,
+	.set_next_event = openrisc_timer_set_next_event,
+	.set_mode = openrisc_timer_set_mode,
 };
 
 static inline void timer_ack(void)
@@ -91,7 +91,7 @@ static inline void timer_ack(void)
 	/* This can be done very simply... we just need to keep the timer
 	   running, so just maintain the CR bits while clearing the rest
 	   of the register
-	*/
+	 */
 	mtspr(SPR_TTMR, SPR_TTMR_CR);
 }
 
@@ -104,23 +104,23 @@ static inline void timer_ack(void)
  * all the exception handler needs to do.
  */
 
-irqreturn_t __irq_entry timer_interrupt(struct pt_regs * regs)
+irqreturn_t __irq_entry timer_interrupt(struct pt_regs *regs)
 {
-        struct pt_regs *old_regs = set_irq_regs(regs);
-        struct clock_event_device *evt = &clockevent_openrisc_timer;
+	struct pt_regs *old_regs = set_irq_regs(regs);
+	struct clock_event_device *evt = &clockevent_openrisc_timer;
 
-        timer_ack();
+	timer_ack();
 
 	/*
 	 * update_process_times() expects us to have called irq_enter().
 	 */
 	irq_enter();
-        evt->event_handler(evt);
+	evt->event_handler(evt);
 	irq_exit();
 
-        set_irq_regs(old_regs);
+	set_irq_regs(old_regs);
 
-        return IRQ_HANDLED;
+	return IRQ_HANDLED;
 }
 
 static __init void openrisc_clockevent_init(void)
@@ -129,15 +129,13 @@ static __init void openrisc_clockevent_init(void)
 				    cpuinfo.clock_frequency, 4);
 
 	/* We only have 28 bits */
-        clockevent_openrisc_timer.max_delta_ns =
-                clockevent_delta2ns((u32)0x0fffffff,
-				    &clockevent_openrisc_timer);
-        clockevent_openrisc_timer.min_delta_ns =
-                clockevent_delta2ns(1, &clockevent_openrisc_timer);
-        clockevent_openrisc_timer.cpumask = cpumask_of(0);
-        clockevents_register_device(&clockevent_openrisc_timer);
+	clockevent_openrisc_timer.max_delta_ns =
+	    clockevent_delta2ns((u32) 0x0fffffff, &clockevent_openrisc_timer);
+	clockevent_openrisc_timer.min_delta_ns =
+	    clockevent_delta2ns(1, &clockevent_openrisc_timer);
+	clockevent_openrisc_timer.cpumask = cpumask_of(0);
+	clockevents_register_device(&clockevent_openrisc_timer);
 }
-
 
 /**
  * Clocksource: Based on OpenRISC timer/counter
@@ -146,27 +144,28 @@ static __init void openrisc_clockevent_init(void)
  * is 32 bits wide and runs at the CPU clock frequency.
  */
 
-static cycle_t openrisc_timer_read(struct clocksource* cs) {
+static cycle_t openrisc_timer_read(struct clocksource *cs)
+{
 	return (cycle_t) mfspr(SPR_TTCR);
 }
 
 static struct clocksource openrisc_timer = {
-	.name		= "openrisc_timer",
-        .rating         = 200,
-	.read		= openrisc_timer_read,
-        .mask           = CLOCKSOURCE_MASK(32),
-        .flags          = CLOCK_SOURCE_IS_CONTINUOUS,
+	.name = "openrisc_timer",
+	.rating = 200,
+	.read = openrisc_timer_read,
+	.mask = CLOCKSOURCE_MASK(32),
+	.flags = CLOCK_SOURCE_IS_CONTINUOUS,
 };
 
 static int __init openrisc_timer_init(void)
 {
-        if (clocksource_register_hz(&openrisc_timer, cpuinfo.clock_frequency))
-                panic("failed to register clocksource");
+	if (clocksource_register_hz(&openrisc_timer, cpuinfo.clock_frequency))
+		panic("failed to register clocksource");
 
 	/* Enable the incrementer: 'continuous' mode with interrupt disabled */
-        mtspr(SPR_TTMR, SPR_TTMR_CR);
+	mtspr(SPR_TTMR, SPR_TTMR_CR);
 
-        return 0;
+	return 0;
 }
 
 void __init time_init(void)
