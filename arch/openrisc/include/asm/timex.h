2601767c5a9356e21b310b8b0a2d20dcb6ac5516
Jonas Bonn <jonas@southpole.se>
2011-06-24 09:03:41 +0200

    time: checkpatch cleanups

--- arch/openrisc/include/asm/timex.h
+++ arch/openrisc/include/asm/timex.h
@@ -24,9 +24,6 @@
 #include <asm/param.h>
 #include <asm/cpuinfo.h>
 
-//#define CLOCK_TICK_RATE	(CONFIG_OPENRISC_SYS_CLK*1000000 / HZ)
-//#define CLOCK_TICK_RATE	(cpuinfo.clock_frequency / HZ)
-
 /* This isn't really used any more */
 #define CLOCK_TICK_RATE 1000
 
