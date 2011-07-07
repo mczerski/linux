d79d1b13057b14e5c2bdde9d36745c6ce79c6e6a
Jonas Bonn <jonas@southpole.se>
2011-07-07 11:46:30 +0200

    nfu: readd sys_mmap
    Not for upstream.
    
    We still need this because we have the legacy syscalls enabled.  Once we
    have a working libc that doesn't need the legacy syscalls, then we can
    discard this patch, too.

--- arch/openrisc/kernel/sys_or32.c
+++ arch/openrisc/kernel/sys_or32.c
@@ -26,6 +26,16 @@
 
 #include <asm/syscalls.h>
 
+asmlinkage long sys_mmap(unsigned long addr, unsigned long len,
+			 unsigned long prot, unsigned long flags,
+			 unsigned long fd, off_t pgoff)
+{
+	if (pgoff & ~PAGE_MASK)
+		return -EINVAL;
+
+	return sys_mmap_pgoff(addr, len, prot, flags, fd, pgoff >> PAGE_SHIFT);
+}
+
 /* These are secondary entry points as the primary entry points are defined in
  * entry.S where we add the 'regs' parameter value
  */
