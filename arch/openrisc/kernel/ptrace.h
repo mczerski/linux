1e160cb71096dd80e7b0d0a166d7b835b9afecd0
Jonas Bonn <jonas@southpole.se>
2011-07-10 22:56:30 +0200

    ptrace: remove header info

--- arch/openrisc/kernel/ptrace.h
+++ arch/openrisc/kernel/ptrace.h
@@ -1,12 +1,3 @@
-/*
- *  linux/arch/openrisc/kernel/ptrace.h
- *
- *  Copyright (C) 2000-2003 Russell King
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
- */
 #include <linux/ptrace.h>
 
 extern void ptrace_cancel_bpt(struct task_struct *);
