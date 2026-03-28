7bb11e9d453da1de6d3a57dc2b10f85b07edff46
Marek Czerski <m.czerski@ap-tech.pl>
2026-03-28 23:48:44 +0100

    amlogic-dvb: clang-format and other formatting changes

--- drivers/media/platform/amlogic/meson-demux/aml_dvb_reg.h
+++ drivers/media/platform/amlogic/meson-demux/aml_dvb_reg.h
@@ -1,22 +1,9 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright (C) 2017 Amlogic, Inc. All rights reserved.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- *
- * Description:
  */
+
 #ifndef _DVB_REG_H_
 #define _DVB_REG_H_
 
@@ -24,4 +11,3 @@
 #include "c_stb_regs_define.h"
 
 #endif
-
